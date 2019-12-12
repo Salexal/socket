06a08bdd80fddbb6a2e5be74263a8d801eadaee9
Salexal <923855206@qq.com>
2019-12-12 15:17:18 +0800

    加入 基础lambda事例

---
+++ src/main/java/wenjing/lambda/ExampleEmployee.java
@@ -0,0 +1,122 @@
+package wenjing.lambda;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.thymeleaf.expression.Lists;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: Salexal.fww
+ * @Date: 2019/12/12 14:03
+ * @Version 1.0
+ * @Type
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+class Employee {
+
+    private String name;
+    private int salary;
+    private String office;
+}
+
+
+/**
+ * @Author: Salexal.fww
+ * @Date: 2019/12/12 14:03
+ * @Version 1.0
+ * @Type
+ */
+public class ExampleEmployee {
+
+    private static List<Employee> employeeList = new ArrayList<>();
+
+    static{
+        employeeList.add(Employee.builder().name("Matt").salary(5000).office("New York").build());
+        employeeList.add(Employee.builder().name("Steve").salary(6000).office("London").build());
+        employeeList.add(Employee.builder().name("Carrie").salary(20000).office("New York").build());
+        employeeList.add(Employee.builder().name("Peter").salary(7000).office("New York").build());
+        employeeList.add(Employee.builder().name("Pat").salary(8000).office("London").build());
+        employeeList.add(Employee.builder().name("Tammy").salary(29000).office("Shanghai").build());
+    }
+
+    public static void main(String[] args) {
+        //anyMatch
+        boolean isMatch = employeeList.stream().anyMatch(employee -> "London".equals(employee.getOffice()));
+        System.out.println(isMatch);
+
+        //所有salary大于6000?
+        boolean matched = employeeList.stream().allMatch(employee -> employee.getSalary()>6000);
+        System.out.println(matched);
+
+        //所有salary大于8000
+        List<Employee> employeeLists =  employeeList.stream().filter((Employee e) -> e.getSalary() > 8000).collect(Collectors.toList());
+        System.out.println(employeeLists);
+
+        //找出工资最高
+        Optional<Employee> hightestSalary = employeeList.stream().max(Comparator.comparingInt(Employee::getSalary));
+        System.out.println(hightestSalary);
+
+        //返回姓名列表
+        List<String> names = employeeList.stream().map(Employee::getName).collect(Collectors.toList());
+        System.out.println(names);
+
+        //List转换成Map
+        Map<String,Employee> employeeMap = employeeList.stream().collect(Collectors.toMap((Employee::getName),(value->value)));
+        employeeMap.forEach((key,value)-> System.out.println(key + " = " + value));
+
+        //统计办公室是New York的个数
+        long officeCount = employeeList.stream().filter(employee -> "New York".equals(employee.getOffice())).count();
+        System.out.println(officeCount);
+
+        //List转换为Set
+        Set<String> officeSet = employeeList.stream().map(Employee::getOffice).collect(Collectors.toSet());
+        System.out.println(officeSet);
+
+        // 查找办公室地点是New York的员工 找到即返回
+        // 注意找不到会报错 @NotNull 使用Optional可以避免 java.util.NoSuchElementException: No value present
+        Optional<Employee> allMatchedEmployees = employeeList.stream().filter(employee -> "New".equals(employee.getOffice())).findAny();
+        System.out.println(allMatchedEmployees);
+
+        //按照工资的降序来列出员工信息
+        List<Employee> sortEmployeeList =  employeeList.stream()
+                .sorted(Comparator.comparing(Employee::getSalary).reversed()).collect(Collectors.toList());
+
+        //按照名字的升序列出员工信息
+        List<Employee> sortEmployeeByName = employeeList.stream().sorted(Comparator.comparing(Employee::getName)).collect(Collectors.toList());
+        System.out.println(sortEmployeeList);
+        System.out.println("按照名字的升序列出员工信息:" + sortEmployeeByName);
+
+        //获取工资最高的前2条员工信息
+        List<Employee> top2EmployeeList= employeeList.stream()
+                .sorted((e1,e2)->Integer.compare(e2.getSalary(),e1.getSalary()))
+                .limit(2)
+                .collect(Collectors.toList());
+        System.out.println(top2EmployeeList);
+
+        //获取平均工资
+        OptionalDouble averageSalary = employeeList.stream().mapToInt(Employee::getSalary).average();
+        System.out.println("平均工资:" + averageSalary);
+
+        //查找New York
+        OptionalDouble averageSalaryByOffice = employeeList.stream().filter(employee -> "New York"
+                .equals(employee.getOffice()))
+                .mapToInt(Employee::getSalary)
+                .average();
+        System.out.println("New York办公室平均工资:" + averageSalaryByOffice);
+
+        // 下面为补充
+
+        // 将相同的office的人员分类存放
+        Map<String,List<Employee>> listMap = employeeList.stream().collect(Collectors.groupingBy(Employee::getOffice));
+        System.out.println("将相同的office的人员分类存放");
+        System.out.println(listMap.keySet());
+        System.out.println(listMap.values());
+    }
+}
